b7f1bb54e75b6d3eaa09a12b654e767bc7723aed
Maxim Koval <110034072+KovalMaxim1989@users.noreply.github.com>
2022-12-06 22:16:32 +0200

    task-1
    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,6 @@
 const body = document.body;
+const numberOfCategories = body.querySelector("#categories").children.length;
+console.log(`Number of categories: ${numberOfCategories}`);
+const liItems = body.querySelectorAll(".item");
+liItems.forEach((liItem) => {  console.log(`Category: ${liItem.firstElementChild.textContent}`)
+ console.log(`Elements: ${liItem.lastElementChild.children.length}`)})
