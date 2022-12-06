9de57c1b41d4683db87dcf459d43197b117d3976
Maxim Koval <110034072+KovalMaxim1989@users.noreply.github.com>
2022-12-06 23:37:33 +0200

    task-2
    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const allItems = ingredients.map(ingredient => {
+  const liItem = document.createElement("li");
+  liItem.textContent = ingredient;
+  liItem.classList.add("item");
+  return liItem;
+});
+console.log(allItems);
+
+const listElem = document.querySelector("#ingredients");
+listElem.append(...allItems);
