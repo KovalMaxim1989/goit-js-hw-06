69731b07e80cd90eb36643d046f4840b2a1567fa
Maxim Koval <110034072+KovalMaxim1989@users.noreply.github.com>
2022-12-09 15:06:52 +0200

    Update task-09.js
    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const buttonElem = document.querySelector('.change-color');
+const bodyElem = document.querySelector('body');
+const spanColor = document.querySelector('.color')
+buttonElem.addEventListener('click', changeColor);
+function changeColor() {
+bodyElem.style.backgroundColor = getRandomHexColor();
+spanColor.textContent = bodyElem.style.backgroundColor;
+}
