2dac0168e8d3172a7887b13c7185977f8d220d57
Maxim Koval <110034072+KovalMaxim1989@users.noreply.github.com>
2022-12-09 14:42:50 +0200

    Update task-08.js
    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
-
+const formElem = document.querySelector(".login-form");
+formElem.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    const mail = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    const formElements = {mail, password};
+    if(mail === "" || password === "") {
+       return alert('Заповніть всі поля');
+    }
+    console.log(formElements);
+    event.currentTarget.reset();
+}
+    
