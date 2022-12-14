5677d14e9fd49f8ecb470172cf1a81ce855c8b23
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2022-12-14 11:30:56 +0200

    Update task-10.js
    start 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const inputEl = document.querySelector('input[type="number"]');
+
+inputEl.addEventListener('change', quantity);
+let amount = 0;
+function quantity(evt) { 
+  amount = Number.parseInt(evt.currentTarget.value);
+ 
+  return amount;
+
+};
+ 
+
+btnCreate.addEventListener('click', createBoxes);
+function createBoxes(amount) { 
+  // let color = '#eb4034';
+  console.log(amount);
+  // for (let i = 1; i <= 5; i += 1 ) { 
+  //   color = getRandomHexColor();
+  //   console.log(amount);
+  // };
+ 
+ 
+}
